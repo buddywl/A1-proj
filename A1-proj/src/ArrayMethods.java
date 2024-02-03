b46b867aa994a63bcad3ebd03029cb5b2e17e020
buddy <kluong@smith.edu>
2024-02-03 12:04:16 -0500

    Initial commit

---
+++ A1-proj/src/ArrayMethods.java
@@ -0,0 +1,107 @@
+public class ArrayMethods {
+    public static float wholeSum(float[] arr){
+        float sum = 0;
+        for(float x : arr) {
+            sum += x;
+        }
+        return sum;
+    }
+
+    public static float sum(float[] arr, int lo, int hi){
+        float sum = 0;
+        for(int i = lo; i < hi; i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    public static float mean(float[] arr, int lo, int hi){
+        if (lo != hi){
+            return sum(arr, lo, hi)/(hi-lo);
+        }
+        else{
+            return 0;
+        }
+
+    }
+
+    public static float mean(float[] arr){
+        if (arr.length != 0){
+            return wholeSum(arr)/arr.length;
+        }
+        else{
+            return 0;
+        }
+
+    }
+
+    public static float min(float[] arr, int lo, int hi){
+        float minVal= 10000;
+        int range = hi - lo;
+
+        if(range != 0){
+            for (int i = lo; i < hi; i++){
+                if (minVal > arr[i]){
+                    minVal = arr[i];
+                }
+
+            }
+            return minVal;
+        }
+        else{
+            return Float.NaN;
+        }
+
+    }
+
+    public static float min(float[] arr){
+        float minVal = 10000;
+
+        if(arr.length != 0){
+            for (int i = 0; i < arr.length; i++){
+                if (minVal > arr[i]){
+                    minVal = arr[i];
+                }
+            }
+            return minVal;
+        }
+        else{
+            return Float.NaN;
+        }
+
+    }
+
+    public static float max(float[] arr, int lo, int hi){
+        float maxVal = -10000;
+        int range = hi - lo;
+
+        if(range != 0){
+            for (int i = lo; i < hi; i++){
+                if (maxVal < arr[i]){
+                    maxVal = arr[i];
+                }
+
+            }
+            return maxVal;
+        }
+        else{
+            return Float.NaN;
+        }    }
+
+    public static float max(float[] arr){
+        float maxVal = -10000;
+
+        if(arr.length != 0){
+            for (int i = 0; i < arr.length; i++){
+                if (maxVal < arr[i]){
+                    maxVal = arr[i];
+                }
+            }
+            return maxVal;
+        }
+        else{
+            return Float.NaN;
+        }
+    }
+
+}
---
+++ A1-proj/src/ClimateQueries.java
@@ -0,0 +1,103 @@
+import java.util.Arrays;
+
+public class ClimateQueries{
+
+    // Below are stubs for each of the methods for the 
+    // climate analysis. You should include both ranged
+    // and unranged methods for each, but only the unranged
+    // are listed below
+    public static void main(String[] args){
+        float[] arr1 = {1.0f, 2.0f, 3.0f, 2.0f};
+        float[] arr2 = {1.0f, -9999.0f, 3.0f, 2.0f};
+
+        boolean[] indicator = new boolean[arr2.length];
+        for (int i = 0; i < indicator.length; i++){
+            indicator[i] = arr2[i] == -9999.0f;
+        }
+        System.out.println(Arrays.toString(indicator));
+
+
+    }
+
+    public static boolean[] isEqualTo(float[] arr, float val){
+        // add your code here 
+        // replace code below with your own return statement
+        boolean[] indicator = new boolean[arr.length];
+        for (int i = 0; i < indicator.length; i++){
+            indicator[i] = arr[i] == val;
+        }
+        return indicator;
+    }
+
+    public static boolean[] logicalNot(boolean[] arr){
+        // add your code here
+        // replace code below with your own return statement
+        boolean[] indicator = new boolean[arr.length];
+        for(int i = 0; i < arr.length; i++){
+            if(!arr[i]){
+                indicator[i] = true;
+            }
+        }
+        return indicator;
+    }
+
+    public static boolean[] logicalAnd(boolean[] arr1, boolean[] arr2){
+        // add your code here
+        // replace code below with your own return statement
+        boolean[] indicator = new boolean[arr1.length];
+        for(int i = 0; i < arr1.length; i++){
+            if(arr1[i] && arr2[i]){
+                indicator[i] = true;
+            }
+            else{
+                indicator[i] = false;
+            }
+        }
+        return indicator;
+    }
+
+    public static boolean[] isGreaterThan(float[] arr, float val){
+        // add your code here
+        // replace code below with your own return statement
+        boolean[] indicator = {false};
+        return indicator;
+    }
+
+    public static boolean[] datesBetween(String[] arr, String date1, String date2){
+        // add your code here
+        // replace code below with your own return statement
+        boolean[] indicator = {false};
+        return indicator;
+    }
+
+    public static float mean(float[] arr, boolean[] indicator, int lo, int hi){
+        // add your code here
+        // replace code below with your own return statement
+        return 0f;
+    }
+
+    public static float mean(float[] arr, boolean[] indicator){
+        // add your code here
+        // replace code below with your own return statement
+        return 0f;
+    }
+
+    public static int count(boolean[] arr){
+        // add your code here
+        // replace code below with your own return statement
+        return 0;
+    }
+
+    public static int findFirst(boolean[] arr){
+        // add your code here
+        // replace code below with your own return statement
+        return -1;
+    }
+
+    public static int[] find(boolean[] arr){
+        // add your code here
+        // replace code below with your own return statement
+        int[] indices = {-1};
+        return indices;
+    }
+}
